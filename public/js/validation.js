05f7ecd150a39644786383399c553b9d67b24617
Carlos Olivares <focus.olivares@gmail.com>
2022-12-08 19:39:00 -0600

    agreando animacion y validacion para confirmar contrasenia

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,4 +1,6 @@
 let inputs = document.querySelector('.form').querySelectorAll('input');
+let inputPassword = document.querySelector('#password');
+let confirmPasswordInput = document.querySelector('#rePassword');
 
 const errorMsgs = {
   name: {
@@ -60,3 +62,20 @@ inputs.forEach(input => {
     }
   });
 });
+
+// Validar password con confirm password
+if (inputPassword != null) {
+  confirmPasswordInput.addEventListener('blur', e => {
+    if (inputPassword.value != '' && confirmPasswordInput.value != '') {
+      let parent = confirmPasswordInput.parentElement;
+      let errorSpan = parent.querySelector('.error-message');
+      if (confirmPasswordInput.value != inputPassword.value) {
+        confirmPasswordInput.classList.add('error');
+        errorSpan.innerHTML = 'Verifique que la contraseña coincida';
+      } else {
+        confirmPasswordInput.classList.remove('error');
+        errorSpan.innerHTML = '';
+      }
+    }
+  });
+}
